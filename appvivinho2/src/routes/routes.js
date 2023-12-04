b93c69a1f9e34016de40bf960f2bcf9e0ecc1b25
Otsuki_Henri <107266141+otsuki1153@users.noreply.github.com>
2023-12-04 00:16:58 -0300

    muitas mudanças graficas

--- appvivinho2/src/routes/routes.js
+++ appvivinho2/src/routes/routes.js
@@ -1,3 +1,23 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+import login from './pages/login';
+import Home from './pages/Home';
+import Pedidos from './pages/Pedidos';
+
+const Stack = createNativeStackNavigator();
+
+function Routes(){
+    return(
+        <Stack.Navigator>
+            <Stack.screen name="Login" component={Login}/>
+            <Stack.screen name="Home" component={Home}/>
+            <Stack.screen name="Pedidos" component={Pedidos}/>
+        </Stack.Navigator>
+    );
+}
+
+export default Routes;
+
 const connection = require ('../database/connection')
 const express = require ('express')
 const router = express.Router()
